60313725431d67e24439180506f89b7b703909bd
Ashna Khan <ashnakhan1277@gmail.com>
2026-07-02 05:05:25 -0700

    Added typing animation and social icons

--- script.js
+++ script.js
@@ -6,3 +6,46 @@ topBtn.onclick = function(){
         behavior:"smooth"
     });
 }
+const words = [
+    "Full Stack Developer",
+    "Frontend Developer",
+    "PHP Developer",
+    "Freelancer"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let currentWord = "";
+let currentChar = "";
+
+const typing = document.querySelector(".typing");
+
+function type(){
+
+    if(wordIndex == words.length){
+        wordIndex = 0;
+    }
+
+    currentWord = words[wordIndex];
+
+    currentChar = currentWord.slice(0,++charIndex);
+
+    typing.textContent = currentChar;
+
+    if(currentChar.length == currentWord.length){
+
+        wordIndex++;
+
+        charIndex = 0;
+
+        setTimeout(type,1000);
+
+    }else{
+
+        setTimeout(type,150);
+
+    }
+
+}
+
+type();
